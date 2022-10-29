4c98c72bfa238c1559e424ebe32e4ddf4057be30
sophia-sam <sampsonjajasophia@gmail.com>
2022-10-29 14:18:06 +0100

    creating html and js files

---
+++ atm.js
@@ -0,0 +1,8 @@
+const pin = 1234
+var input = prompt("please input your pin")
+if (pin===input) {
+    console.log(`you have typed the correct pin`)
+}
+ else {
+     console.log(`you have typed the wrong pin`)
+}
